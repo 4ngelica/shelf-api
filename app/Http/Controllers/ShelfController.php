ec98021e780a235dd42f4a6b04df33cf2e4503eb
Angel <angelica.nunes@usp.br>
2021-11-18 00:23:14 -0300

    Initial commit

---
+++ app/Http/Controllers/ShelfController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Arr;
+
+class ShelfController extends Controller
+{
+
+  public function handle()
+  {
+
+  }
+
+  public function index()
+  {
+
+  }
+
+  public function show()
+  {
+
+  }
+}
